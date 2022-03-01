737a708c761c180ceabab9509d982730b941ecc3
Marko Vastag <markovastag@vivaldi.net>
2022-03-01 14:08:21 +0100

    bugfix find_files

--- memobrain/utils.py
+++ memobrain/utils.py
@@ -3,7 +3,7 @@ import glob
 
 def find_files(file_ext, cpath='.'):
     all_paths = []
-    for file_path in Path(cpath).rglob(file_ext):
+    for file_path in Path(cpath).rglob(f"*.{file_ext}"):
         all_paths.append(file_path)
 
     return all_paths
