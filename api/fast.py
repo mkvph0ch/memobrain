cd052d65c8fc7dadaa706fca8752065958ad30f5
cynthias13w <mcynsiew@gmail.com>
2022-03-08 15:58:50 +0100

    Added files

--- api/fast.py
+++ api/fast.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import date
+import datetime
+from datetime import date, datetime
 import pandas as pd
 import joblib
+
+### TRYING TO DOWNLOAD A MODEL FROM THE CLOUD    ###
+
+# from google.cloud import storage
+# from sklearn.externals import joblib
+# from tempfile import TemporaryFile
+
+# storage_client = storage.Client()
+# bucket_name=<bucket name>
+# model_bucket='model.joblib'
+
+# bucket = storage_client.get_bucket(bucket_name)
+# #select bucket file
+# blob = bucket.blob(model_bucket)
+# with TemporaryFile() as temp_file:
+#     #download blob into temp file
+#     blob.download_to_file(temp_file)
+#     temp_file.seek(0)
+#     #load into joblib
+#     model=joblib.load(temp_file)
 
 app = FastAPI()
 
@@ -20,26 +41,35 @@ def index():
     return {"greeting": "Hello world"}
 
 @app.get("/predict")
-def predict(sex='F', age = 20, Educ='Some college', SES=1, MMSE=25, eTIV=1.2, nWBV=0.8, ASF=0.5):
+def predict(sex='F', birthday = "19951201", EDUC=12, SES=1, MMSE=25, eTIV=1.2, nWBV=0.8, ASF=0.5):
 
     # Encode male and female
-    sex = sex.replace('M', '1')
-    sex = sex.replace('F', '0')
+    # sex = sex.replace('M', '1')
+    # sex = sex.replace('F', '0')
 
     # Encode Education
-    education = {'Lower than high school': '1',
-                'High school graduate': '2',
-                'Some college': '3',
-                'College graduate': '4',
-                'Beyond college': '5'}
+    # education = {'Lower than high school': '1',
+    #             'High school graduate': '2',
+    #             'Some college': '3',
+    #             'College graduate': '4',
+    #             'Beyond college': '5'}
 
-    for k, v in education.items():
-        Educ = Educ.replace(k, v)
+    # for k, v in education.items():
+    #     EDUC = EDUC.replace(k, v)
+
+    # Calculate age from birthday
+    # birthday = date(birthday)
+    # today = date.today()
+    # age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+
+    birthday = datetime(year=int(birthday[0:4]), month=int(birthday[4:6]), day=int(birthday[6:8]))
+    today = date.today()
+    age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
 
     result = {
-    "M/F": int(sex),
-    "Age": int(age),
-    "Educ": str(Educ),
+    "M/F": str(sex),
+    "Age": float(age),
+    "EDUC": float(EDUC),
     "SES": float(SES),
     "MMSE": float(MMSE),
     "eTIV": float(eTIV),
@@ -48,15 +78,17 @@ def predict(sex='F', age = 20, Educ='Some college', SES=1, MMSE=25, eTIV=1.2, nW
     }
 
     X_pred = pd.DataFrame(result, index=[0])
+    print(X_pred.dtypes)
 
-    pipeline = joblib.load("model.joblib")
+    pipeline = joblib.load("../MemoBrainModel/model.joblib")
+    #pipeline = joblib.load("gs://memobrain2/models/memobrain/v1/model.joblib")
     prediction = pipeline.predict(X_pred)
 
-    prediction_result =  {"diagnosis": prediction[0]}
+    prediction_result =  {"diagnosis": str(prediction[0])}
 
-    return X_pred
+    return prediction_result
 
 if __name__ == "__main__":
     print("It works")
-    #result = predict()
-    #print(result)
+    test = predict()
+    print(test)
